5412e1300f7765e7cbf74d38f81ae897ceb64689
j-novaes <joaovictor.nb21@hotmail.com>
2022-02-16 21:40:11 +0000

    Edited CollectionBar

--- src/components/CollectionBar/CollectionBar.js
+++ src/components/CollectionBar/CollectionBar.js
@@ -13,11 +13,27 @@ export default function CollectionBar() {
     return (
         <section className='collectionBar'>
             <h2>Your Collection</h2>
-            <ul>
-            </ul>
             <div className='collectionDisplay'>
-                    {myCollection.map((item, index) => <PalleteBox id={item.id} colors={item.colors} size='small'/>)}
+                    {myCollection.map((item, index) => <PalleteBox id={item.id} key={index} colors={item.colors} size='small'/>)}
             </div>
+        <style jsx>{`
+            .collectionBar {
+                width: 250px;
+                height: 100vh;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                position: fixed;
+                right: 0px;
+                border-left: 1px solid rgba(0,0,0,0.1);
+            }
+
+            .collectionDisplay {
+                display: grid;
+                grid-template-columns: repeat(4, 1fr);
+                grid-column-gap: 7px;
+            }
+        `}</style>
         </section>
     )
 }
